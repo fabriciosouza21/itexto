1aec95663c1673997cf8f33d04341ae087e06665
fabricio21777 <fabriciosouza21777@gmail.com>
2021-10-14 16:07:00 -0300

    conexão com o banco de dados

---
+++ src/main/java/com/fsm/itext/ItextApplication.java
@@ -0,0 +1,13 @@
+package com.fsm.itext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItextApplication.class, args);
+	}
+
+}
